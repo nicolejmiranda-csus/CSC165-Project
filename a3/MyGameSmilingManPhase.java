97f98cc763ed7570837c422d8e3d1deba2fc612e
Givin Yang <collegegivin18@gmail.com>
2026-05-04 19:45:19 -0700

    add SmilingMan behavior phase enum

---
+++ a3/MyGameSmilingManPhase.java
@@ -0,0 +1,19 @@
+package a3;
+
+enum MyGameSmilingManPhase {
+    HIDDEN,
+    WANDER_IDLE,
+    WANDER_WALK,
+    FAKE_IDLE,
+    GATHER_TARGETS,
+    TARGET_WAVE,
+    RUN_TRANSITION,
+    RUN_INITIAL,
+    CHASE,
+    HIT_OUTRO,
+    HIT_STAND,
+    HIT_WAVE,
+    HIT_RETRANSITION,
+    HIT_REINITIAL,
+    REMOTE_CONTROLLED
+}
