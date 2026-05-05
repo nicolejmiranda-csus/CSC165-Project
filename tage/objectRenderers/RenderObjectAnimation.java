3ee705cf21d15b2c69609ca132a63bcdc72757b1
Givin Yang <collegegivin18@gmail.com>
2026-05-04 19:32:57 -0700

    upload skeletal animation matrices with SSBOs and support opacity

--- tage/objectRenderers/RenderObjectAnimation.java
+++ tage/objectRenderers/RenderObjectAnimation.java
@@ -25,16 +25,19 @@ public class RenderObjectAnimation
 
 	// allocate variables for display() function
 	private FloatBuffer vals = Buffers.newDirectFloatBuffer(16);
-	private FloatBuffer valsIT = Buffers.newDirectFloatBuffer(9);
+	private FloatBuffer skinVals = Buffers.newDirectFloatBuffer(AnimatedShape.MAX_SKIN_BONES * 16);
+	private FloatBuffer skinITVals = Buffers.newDirectFloatBuffer(AnimatedShape.MAX_SKIN_BONES * 16);
 	private Matrix4f pMat = new Matrix4f();  // perspective matrix
 	private Matrix4f vMat = new Matrix4f();  // view matrix
 	private Matrix4f mMat = new Matrix4f();  // model matrix
 	private Matrix4f invTrMat = new Matrix4f(); // inverse-transpose
-	private int mLoc, vLoc, pLoc, nLoc, eLoc, oLoc, hLoc, tfLoc, tLoc, lLoc, fLoc, sLoc, cLoc;
+	private int mLoc, vLoc, pLoc, nLoc, eLoc, oLoc, hLoc, tfLoc, tLoc, lLoc, fLoc, sLoc, cLoc, isTransparentLoc, alphaLoc, flipLoc;
 	private int globalAmbLoc,mambLoc,mdiffLoc,mspecLoc,mshiLoc;
-	private int skinMatLoc, skinMatITLoc;
 	private int hasSolidColor, hasTex, thisTexture, defaultTexture, tiling, tilingOption, tileFactor, heightMapped;
-	private int isEnvMapped, hasLighting, activeSkyBoxTexture, heightMapTexture;
+	private int isEnvMapped, hasLighting, activeSkyBoxTexture, heightMapTexture, isTransparent;
+	private float alpha;
+	private int[] skinSSBO = new int[1];
+	private int[] skinITSSBO = new int[1];
 
 	/** for engine use only. */
 	public RenderObjectAnimation(Engine e)
@@ -61,6 +64,9 @@ public class RenderObjectAnimation
 		oLoc = gl.glGetUniformLocation(renderingProgram, "hasLighting");
 		sLoc = gl.glGetUniformLocation(renderingProgram, "solidColor");
 		cLoc = gl.glGetUniformLocation(renderingProgram, "color");
+		isTransparentLoc = gl.glGetUniformLocation(renderingProgram, "isTransparent");
+		alphaLoc = gl.glGetUniformLocation(renderingProgram, "alpha");
+		flipLoc = gl.glGetUniformLocation(renderingProgram, "flipNormal");
 		hLoc = gl.glGetUniformLocation(renderingProgram, "heightMapped");
 		lLoc = gl.glGetUniformLocation(renderingProgram, "num_lights");
 		fLoc = gl.glGetUniformLocation(renderingProgram, "fields_per_light");
@@ -100,6 +106,13 @@ public class RenderObjectAnimation
 			hasLighting = 1;
 		else
 			hasLighting = 0;
+
+		if (go.getRenderStates().isTransparent())
+			isTransparent = 1;
+		else
+			isTransparent = 0;
+
+		alpha = go.getRenderStates().getOpacity();
 		
 		gl.glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, (engine.getLightManager()).getLightSSBO());
 
@@ -116,6 +129,9 @@ public class RenderObjectAnimation
 		gl.glUniform1i(oLoc, hasLighting);
 		gl.glUniform1i(sLoc, hasSolidColor);
 		gl.glUniform3fv(cLoc, 1, ((go.getRenderStates()).getColor()).get(vals));
+		gl.glUniform1i(isTransparentLoc, isTransparent);
+		gl.glUniform1f(alphaLoc, alpha);
+		gl.glUniform1f(flipLoc, 1.0f);
 		gl.glUniform1i(hLoc, heightMapped);
 		tileFactor = (go.getRenderStates()).getTileFactor();
 		gl.glUniform1i(tfLoc, tileFactor);
@@ -127,12 +143,7 @@ public class RenderObjectAnimation
 		gl.glProgramUniform4fv(renderingProgram, mspecLoc, 1, go.getShape().getMatSpe(), 0);
 		gl.glProgramUniform1f(renderingProgram, mshiLoc, go.getShape().getMatShi());
 		
-		for (int i=0; i<boneCount; i++)
-		{	skinMatLoc = gl.glGetUniformLocation(renderingProgram, "skin_matrices["+i+"]");
-			skinMatITLoc = gl.glGetUniformLocation(renderingProgram, "skin_matrices_IT["+i+"]");
-			gl.glUniformMatrix4fv(skinMatLoc, 1, false, vals.put(0, skinMats[i].toFloatArray()));
-			gl.glUniformMatrix3fv(skinMatITLoc, 1, false, vals.put(0, skinMatsIT[i].toFloatArray()));
-		}
+		uploadSkinMatrices(gl, skinMats, skinMatsIT, boneCount);
 
 		gl.glBindBuffer(GL_ARRAY_BUFFER, go.getShape().getVertexBuffer());
 		gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
@@ -191,7 +202,58 @@ public class RenderObjectAnimation
 		gl.glEnable(GL_DEPTH_TEST);
 		gl.glDepthFunc(GL_LEQUAL);
 
-		gl.glDrawArrays(GL_TRIANGLES, 0, go.getShape().getNumVertices());
+		if (isTransparent == 1)
+		{	gl.glEnable(GL_BLEND);
+			gl.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			gl.glBlendEquation(GL_FUNC_ADD);
+			gl.glDepthMask(false);
+			gl.glDisable(GL_CULL_FACE);
+			gl.glDrawArrays(GL_TRIANGLES, 0, go.getShape().getNumVertices());
+			gl.glDepthMask(true);
+			gl.glDisable(GL_BLEND);
+		}
+		else
+		{	gl.glDisable(GL_BLEND);
+			gl.glDisable(GL_CULL_FACE);
+			gl.glDrawArrays(GL_TRIANGLES, 0, go.getShape().getNumVertices());
+		}
+	}
+
+	private void uploadSkinMatrices(GL4 gl, tage.rml.Matrix4[] skinMats, tage.rml.Matrix3[] skinMatsIT, int boneCount)
+	{	int uploadCount = java.lang.Math.min(boneCount, AnimatedShape.MAX_SKIN_BONES);
+		uploadCount = java.lang.Math.min(uploadCount, skinMats.length);
+		uploadCount = java.lang.Math.min(uploadCount, skinMatsIT.length);
+
+		if (skinSSBO[0] == 0)
+			gl.glGenBuffers(1, skinSSBO, 0);
+		if (skinITSSBO[0] == 0)
+			gl.glGenBuffers(1, skinITSSBO, 0);
+
+		skinVals.clear();
+		skinITVals.clear();
+		for (int i = 0; i < uploadCount; i++)
+		{	skinVals.put(skinMats[i].toFloatArray());
+			putMatrix3AsMatrix4(skinITVals, skinMatsIT[i]);
+		}
+		skinVals.flip();
+		skinITVals.flip();
+
+		gl.glBindBuffer(GL_SHADER_STORAGE_BUFFER, skinSSBO[0]);
+		gl.glBufferData(GL_SHADER_STORAGE_BUFFER, skinVals.limit() * 4, skinVals, GL_DYNAMIC_DRAW);
+		gl.glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, skinSSBO[0]);
+
+		gl.glBindBuffer(GL_SHADER_STORAGE_BUFFER, skinITSSBO[0]);
+		gl.glBufferData(GL_SHADER_STORAGE_BUFFER, skinITVals.limit() * 4, skinITVals, GL_DYNAMIC_DRAW);
+		gl.glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, skinITSSBO[0]);
+		gl.glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+	}
+
+	private void putMatrix3AsMatrix4(FloatBuffer buffer, tage.rml.Matrix3 matrix)
+	{	float[] values = matrix.toFloatArray();
+		buffer.put(values[0]).put(values[1]).put(values[2]).put(0.0f);
+		buffer.put(values[3]).put(values[4]).put(values[5]).put(0.0f);
+		buffer.put(values[6]).put(values[7]).put(values[8]).put(0.0f);
+		buffer.put(0.0f).put(0.0f).put(0.0f).put(1.0f);
 	}
 	
 	private FloatBuffer directFloatBuffer(float[] values)
