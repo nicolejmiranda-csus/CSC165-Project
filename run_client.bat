8aecf69c147ed2b6676ce252c235195596cfb9d7
Givin Yang <collegegivin18@gmail.com>
2026-05-12 14:43:47 -0700

    Clean game run scripts

--- run_client.bat
+++ run_client.bat
@@ -1,7 +1,6 @@
-
 java --add-exports java.base/java.lang=ALL-UNNAMED ^
      --add-exports java.desktop/sun.awt=ALL-UNNAMED ^
      --add-exports java.desktop/sun.java2d=ALL-UNNAMED ^
      -Dsun.java2d.d3d=false ^
      -Dsun.java2d.uiScale=1 ^
-     running_Dead.MyGame AUTO %1
+     running_Dead.MyGame AUTO %*
